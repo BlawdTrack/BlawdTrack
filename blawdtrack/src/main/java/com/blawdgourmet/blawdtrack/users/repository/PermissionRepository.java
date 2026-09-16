7f35dcc086138f9327555e72e2e3ce0713bef772
solorzano923 <alvaro.solorzanoelizondo@gmail.com>
2026-09-15 21:22:05 -0600

    Delete blawdtrack directory

--- blawdtrack/src/main/java/com/blawdgourmet/blawdtrack/users/repository/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blawdgourmet.blawdtrack.users.repository;
-
-import com.blawdgourmet.blawdtrack.users.model.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-}
