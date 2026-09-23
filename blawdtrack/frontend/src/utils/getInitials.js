6f472249b9b088de6b8aaacb10384bb8aa9a902c
Genesis Silesky Araya <sileskyg26@gmail.com>
2026-09-23 11:34:52 -0600

    chore(frontend): eliminar blawdtrack/frontend y rescatar documentación a docs/ #TBD (#57)

--- blawdtrack/frontend/src/utils/getInitials.js
+++
@@ -1,9 +0,0 @@
-export const getInitials = (fullName = '') =>
-  fullName
-    .trim()
-    .split(/\s+/)
-    .filter(Boolean)
-    .slice(0, 2)
-    .map((part) => part.charAt(0))
-    .join('')
-    .toUpperCase();
